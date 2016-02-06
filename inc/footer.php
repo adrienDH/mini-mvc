15d201678baefede3d141d7aa58b0d97f69f1bfc
adrien <adrien.dhermy@gmail.com>
2016-02-06 01:06:13 +0100

    #FIX : inclusion des fichiers JS

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 <script type='text/javascript' src='./resources/bootstrap/js/bootstrap.min.js'></script>
 
 <?php foreach($mainController->getJsPaths() as $js): ?>
-	<?= "<script type='text/javascript' src='" . $js . "'></script>" ?>
+	<?= "<script type='text/javascript' src='" . $js . ".js'></script>" ?>
 <?php endforeach; ?>
 
 </body>
