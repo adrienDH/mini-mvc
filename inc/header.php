be10142cca022df1c85146337443df445e3e6a8c
adrien <adrien.dhermy@gmail.com>
2016-02-22 01:10:49 +0100

    inclusion des métas dynamique

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,16 @@
 <html>
-<title>home</title>
+<head>
 
-<link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="./resources/css/common/common.css">
+	<title><?= $mainController->getMetaTitle() ?></title>
+	<meta name="description" content="<?= $mainController->getMetaDescription() ?>" />
+	<meta name="keywords" content="<?= $mainController->getMetaKeywords() ?>"> 
 
-
-<?php foreach($mainController->getCssPaths() as $css): ?>
-	<?= "<link rel='stylesheet' type='text/css' href='" . $css .  ".css'>" ?>
-<?php endforeach;?>
+	<link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="./resources/css/common/common.css">
+	
+	
+	<?php foreach($mainController->getCssPaths() as $css): ?>
+		<?= "<link rel='stylesheet' type='text/css' href='" . $css .  ".css'>" ?>
+	<?php endforeach;?>
+</head>
 <body>
